b6dd544880631aa8a077ff096c3424eb30a0f33f
JiyouSeo <seojiyu1113@gmail.com>
2022-06-22 13:06:26 +0900

    coordconv

--- model/nn.py
+++ model/nn.py
@@ -25,6 +25,7 @@ class GroupNorm32(nn.GroupNorm):
         return super().forward(x.float()).type(x.dtype)
 
 
+
 def conv_nd(dims, *args, **kwargs):
     """
     Create a 1D, 2D, or 3D convolution module.
